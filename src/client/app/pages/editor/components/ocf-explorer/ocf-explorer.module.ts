922b0423efb998b8436bb29e06a0589518e29100
Salvatore Iovene <salvatore.iovene@intel.com>
2017-01-31 15:02:15 +0200

    Add connection form to OCF explorer

--- src/client/app/pages/editor/components/ocf-explorer/ocf-explorer.module.ts
+++ src/client/app/pages/editor/components/ocf-explorer/ocf-explorer.module.ts
@@ -1,13 +1,17 @@
 // Core modules
-import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { NgModule } from '@angular/core';
+
+// 3rd party modules
+import { Angular2FontawesomeModule } from 'angular2-fontawesome/angular2-fontawesome';
 
 // This module
 import { OcfExplorerComponent } from './ocf-explorer.component';
 
 
 @NgModule({
-    imports: [CommonModule],
+    imports: [CommonModule, FormsModule, Angular2FontawesomeModule],
     declarations: [OcfExplorerComponent],
     exports: [OcfExplorerComponent]
 })
