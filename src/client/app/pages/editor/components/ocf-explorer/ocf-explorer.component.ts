d5a51e86567e2935e4fe72737477021435428b3e
Salvatore Iovene <salvatore.iovene@intel.com>
2017-01-31 15:02:15 +0200

    Add connection form to OCF explorer

--- src/client/app/pages/editor/components/ocf-explorer/ocf-explorer.component.ts
+++ src/client/app/pages/editor/components/ocf-explorer/ocf-explorer.component.ts
@@ -1,5 +1,13 @@
 import { Component } from '@angular/core';
 
+import { OcfApiService } from './ocf.api.services';
+
+
+export interface OcfServer {
+    ip: string;
+    port: number;
+};
+
 
 @Component({
     moduleId: module.id,
@@ -7,4 +15,42 @@ import { Component } from '@angular/core';
     templateUrl: 'ocf-explorer.component.html',
     styleUrls: ['ocf-explorer.component.css']
 })
-export class OcfExplorerComponent { }
+export class OcfExplorerComponent {
+    public server: OcfServer = {
+        ip: '192.168.0.102',
+        port: 8000
+    };
+
+    public constructor(private ocfApiService: OcfApiService) {
+    }
+
+    // tslint:disable-next-line:no-unused-variable
+    public isValidIPAddress(): boolean {
+        let ip = this.server.ip;
+        return /^(?!0)(?!.*\.$)((1?\d?\d|25[0-5]|2[0-4]\d)(\.|$)){4}$/.test(ip);
+    }
+
+    // tslint:disable-next-line:no-unused-variable
+    public isValidPort(): boolean {
+        return this.server.port > 0 && this.server.port < 65536;
+    }
+
+    // tslint:disable-next-line:no-unused-variable
+    public mayConnect(): boolean {
+        return this.isValidIPAddress() && this.isValidPort();
+    }
+
+    // tslint:disable-next-line:no-unused-variable
+    public onConnectClicked(event: any): void {
+        event.preventDefault();
+
+        this.ocfApiService.setBaseUrl(
+            'http://' + this.server.ip +
+                  ':' + this.server.port);
+        this.ocfApiService.getResources().$observable.subscribe(
+            (resources: any[]) => {
+                console.log(resources);
+            }
+        );
+    }
+}
---
+++ src/client/app/pages/editor/components/ocf-explorer/ocf.api.services.ts
@@ -0,0 +1,45 @@
+import { Injectable } from '@angular/core';
+
+import {
+    Resource,
+    ResourceAction,
+    ResourceMethod,
+    ResourceParams
+} from 'ng2-resource-rest';
+
+
+export class RestClient extends Resource {
+    private baseUrl = '';
+
+    public setBaseUrl(url: string): void {
+        this.baseUrl = url;
+    }
+
+    public getUrl(methodOptions?: any): string | Promise<string> {
+        let resPath = super.getUrl();
+        return this.baseUrl + resPath;
+    }
+}
+
+
+// Interfaces
+
+export interface IQueryInput {
+  page?: number;
+  perPage?: number;
+};
+
+
+// Services
+
+@Injectable()
+@ResourceParams({
+  url: '/api/oic'
+})
+export class OcfApiService extends RestClient {
+  @ResourceAction({
+    isArray: true,
+    path: '/res'
+  })
+  getResources: ResourceMethod<IQueryInput, any[]>;
+}
