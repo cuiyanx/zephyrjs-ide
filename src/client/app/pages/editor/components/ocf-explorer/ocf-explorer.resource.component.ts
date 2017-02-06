bf9f71c4532ede6990c9a2e685c055d3b51fb4b6
Salvatore Iovene <salvatore.iovene@intel.com>
2017-02-06 13:34:32 +0200

    (ocf-explorer) Implement timeout when getting properties

--- src/client/app/pages/editor/components/ocf-explorer/ocf-explorer.resource.component.ts
+++ src/client/app/pages/editor/components/ocf-explorer/ocf-explorer.resource.component.ts
@@ -1,10 +1,13 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 import { IOcfResource } from './ocf-explorer.api.services';
+
+import { OcfApiService } from './ocf-explorer.api.services';
 
 
 export interface OcfResource extends IOcfResource {
     properties?: any;
+    error?: string;
 };
 
 
@@ -14,6 +17,37 @@ export interface OcfResource extends IOcfResource {
     templateUrl: 'ocf-explorer.resource.component.html',
     styleUrls: ['ocf-explorer.resource.component.css']
 })
-export class OcfResourceComponent {
+export class OcfResourceComponent implements OnInit {
     @Input('resource') resource: OcfResource;
+
+    public constructor(private ocfApiService: OcfApiService) {
+    }
+
+    public ngOnInit() {
+        this.getProperties();
+    }
+
+    // tslint:disable-next-line:no-unused-variable
+    public getProperties() {
+        this.resource.properties = null;
+        this.resource.error = null;
+
+        let request = this.ocfApiService.getResource(this.resource);
+        let timeout = setTimeout(() => {
+            request.$abortRequest();
+            this.resource.error = 'TIMEOUT';
+        }, 5000);
+
+        request.$observable.subscribe((response: any) => {
+            clearTimeout(timeout);
+            this.resource.properties = response.properties;
+            this.resource.error = null;
+        });
+    }
+
+    // tslint:disable-next-line:no-unused-variable
+    public onRetryClicked() {
+        this.getProperties();
+        return false;
+    }
 }
