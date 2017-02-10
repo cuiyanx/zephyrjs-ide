a7897faa3845db08a86ebf8439b724314ced61b1
Salvatore Iovene <salvatore.iovene@intel.com>
2017-02-10 14:30:14 +0200

    Remove double copy of ClearSans fonts

--- tools/config/project.config.ts
+++ tools/config/project.config.ts
@@ -12,9 +12,10 @@ export class ProjectConfig extends SeedConfig {
   PROJECT_TASKS_DIR = join(process.cwd(), this.TOOLS_DIR, 'tasks', 'project');
   GIT_REV: string = '';
 
+  // Bootstrap's glyphicons and FontAwesome expect fonts to be in `../fonts`
+  // as a path that's relative to the CSS file, so we need to copy them.
   FONTS_DEST = `${this.APP_DEST}/fonts`;
   FONTS_SRC = [
-      `${this.APP_SRC}/assets/fonts/**`,
       'node_modules/bootstrap/dist/fonts/**',
       'node_modules/font-awesome/fonts/**'
   ];
